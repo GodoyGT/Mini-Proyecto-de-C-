fd30798ba7b8c5c99b063f25abc9d0786e0c4c77
Galo_Godoy <galouniversidad2008@gmail.com>
2026-05-17 18:12:06 -0500

    MINI PROYECTO EN C++ - Iniciar creando un buen encabezado a nuestro ejercicio

---
+++ Sistema_de_Control_de_Empleados.cpp
@@ -0,0 +1,17 @@
+/*
+CLASE 11 
+MINI PROYECTO DE C++ 
+
+Tema: SISTEMA DE CONTROL DE EMPLEADOS
+
+Autor: Godoy Galo
+Fecha: 17 de mayo de 2026
+Enunciado: Una empresa desea llevar un control básico de sus empleados mediante un programa en C++.
+
+El sistema permitirá registrar empleados almacenando datos como nombre, edad y sueldo. 
+Además, el programa contará con un menú en consola para mostrar empleados, buscar e
+mpleados, calcular el sueldo total y mostrar el empleado con mayor y menor sueldo.
+
+La información deberá almacenarse utilizando arreglos y el programa deberá estar organizado mediante 
+funciones y validaciones básicas.
+*/
